d89796e6e91f632c0afcd3e00187e1842e3e661a
David Hatch <dhatch387@gmail.com>
2013-11-15 22:16:20 -0500

    Convert UserResource to use SQLAlchemy models.

--- workspaces/api/resources/user.py
+++ workspaces/api/resources/user.py
@@ -3,10 +3,8 @@ from collections import namedtuple
 from flask.ext.restful import fields
 
 from .model import ModelResource
+from ...models import User
 
-
-# temporary shim to hold in for database
-User = namedtuple('user', ['first_name', 'last_name', 'username', 'id'])
 
 class UserResource(ModelResource):
     """Represents a user.
@@ -26,21 +24,11 @@ class UserResource(ModelResource):
         'id': fields.Integer,
     }
     
+    model = User
+    
     def get_list(self, **kwargs):
         if 'current_user' in kwargs:
             # TODO Redo when using database
             return self.get_detail(1)
         else:
             return super(UserResource, self).get_list(**kwargs)
-            
-    def get_objects(self):
-        return [
-            User(first_name="David", 
-                 last_name="Hatch", 
-                 username="david.hatch",
-                 id=1),
-            User(first_name="Michael", 
-                 last_name="Menz",
-                 username="michael.menz",
-                 id=2)
-        ]
