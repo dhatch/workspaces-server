10a1108a6c577f508874ee10a8883498e924a651
David Hatch <dhatch387@gmail.com>
2013-11-15 23:55:04 -0500

    Students-classes relationship.

--- workspaces/models.py
+++ workspaces/models.py
@@ -3,10 +3,10 @@ from flask.ext import login, security
 from .core import db
 from .classes.models import *
 
-# schedules = db.Table('schedules',
-#     db.Column('class_id', db.Integer, db.ForeignKey('class.id')),
-#     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
-# )
+schedules = db.Table('schedules',
+    db.Column('class_id', db.Integer, db.ForeignKey('class.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('workspaces_user.id'))
+)
 
 # friend_lists = db.Table('friend_lists',
 #     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
@@ -24,8 +24,9 @@ class User(db.Model, security.UserMixin):
     first_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64), index=True)
 
-    # classes = db.relationship('Schedule', secondary=schedules,
-    #     backref=db.backref('users', lazy='dynamic'))
+    classes = db.relationship('Class', secondary=schedules,
+        backref=db.backref('students', lazy='dynamic'), lazy='dynamic')
+        
     # friend_list = db.relationship('Friend_list', secondary=friend_lists,
     #     backref=db.backref('users', lazy='dynamic'))
     
